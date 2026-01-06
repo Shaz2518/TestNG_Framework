fbd68eb4165e97bde966611a6c702fb9a4e874e0
ShaziyaShaikh <shaziyashaikh2518@outlook.com>
2026-01-06 20:51:32 +0530

    TestNG Project

---
+++ TestNG/src/test/java/firstProgramPackage/TestNGProgram.java
@@ -0,0 +1,11 @@
+package firstProgramPackage;
+
+import org.testng.annotations.Test;
+
+public class TestNGProgram {
+
+	@Test
+	public static void testNGMethod() {
+		System.out.println("This is a program with TestNG");
+	}
+}
