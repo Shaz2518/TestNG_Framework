332333dc44fa3887bce4732c100c1323507e35f1
ShaziyaShaikh <shaziyashaikh2518@outlook.com>
2026-01-06 20:18:41 +0530

    TestNG Project

---
+++ src/basicProgram/JavaProgram_HelloWorld.java
@@ -0,0 +1,11 @@
+package basicProgram;
+
+public class JavaProgram_HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello and Welcome to TestNG Learning");
+
+	}
+
+}
