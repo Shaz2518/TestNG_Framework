572b4dee9a15ab53e94e6d54bd77c02bcc65b17f
ShaziyaShaikh <shaziyashaikh2518@outlook.com>
2026-01-11 20:19:33 +0530

    Data Provider

---
+++ TestNG/src/test/java/testNGAnnotations/DataProviderDemo.java
@@ -0,0 +1,28 @@
+package testNGAnnotations;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviderDemo {
+
+	@Test(dataProvider = "Login")
+	public void loginData(String username, String password) {
+		System.out.println("Data Provider Demo");
+		
+		System.out.println(username + " " + password);
+	}
+	
+	
+	@DataProvider(name = "Login")
+	public Object[][] dataProviderMethod(){
+		
+		return new Object[][]
+				{
+			
+			{"user1" ,"password1"},
+			{"user2" , "password2"},
+			{"user3" , "password3"}
+			
+		};
+	}
+}
