609c4e4204fb7255cd1f2b2c958663dcb2f7b464
ShaziyaShaikh <shaziyashaikh2518@outlook.com>
2026-01-13 14:06:03 +0530

    Parallel Execution - Suite and Test level

---
+++ TestNG/src/test/java/parallelExecution/ParallelExecution.java
@@ -0,0 +1,32 @@
+package parallelExecution;
+
+import org.testng.annotations.Test;
+
+public class ParallelExecution {
+	
+	
+	@Test
+	public void parallelExec1() {
+		System.out.println("Method 1: " + Thread.currentThread().getId());
+	}
+	
+	@Test
+	public void parallelExec2() {
+		System.out.println("Method 2: " + Thread.currentThread().getId());
+	}
+	
+	@Test
+	public void parallelExec3() {
+		System.out.println("Method 3: " + Thread.currentThread().getId());
+	}
+	
+	@Test
+	public void parallelExec4() {
+		System.out.println("Method 4: " + Thread.currentThread().getId());
+	}
+
+	@Test
+	public void parallelExec5() {
+		System.out.println("Method 5: " + Thread.currentThread().getId());
+	}
+}
